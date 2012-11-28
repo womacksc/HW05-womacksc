df48120ac586d832ddb9820cf2a044de748daa2c
Shiloh Womack <womacksc@muohio.edu>
2012-11-28 09:30:51 -0500

    Partly done

---
+++ HW05-womacksc/HW05-womacksc/ListGraph.cpp
@@ -0,0 +1,57 @@
+#include "ListGraph.h"
+
+
+ListGraph::ListGraph(int numNodes){	
+	num_edges = 0;	
+	edgeList.resize(numNodes);	
+}
+
+ListGraph::~ListGraph(){
+}	
+	
+void ListGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight){	
+		
+	if ((0 <= u < edgeList.size()) && (0 <= v < edgeList.size()) && (u != v) && (weight > 0)){		
+		bool identical = false;
+	
+		//Check if edge already exists in u's list
+		for(std::list<NWPair>::const_iterator iter = edgeList.[u].begin(); iter != edgeList[u].end(); iter++){
+				//http://stdcxx.apache.org/doc/stdlibug/2-2.html and class notes
+				
+			if((iter->first == v) && (iter->second == weight)){			
+				identical = true;			
+				}		
+		}
+			
+		if (identical == false){			
+			edgeList.at(u).push_back(NWPair(v,weight));	//adds edge to u's list		
+			edgeList.at(v).push_back(NWPair(u,weight));	
+			//push_back understanding grabbed from http://www.yolinux.com/TUTORIALS/LinuxTutorialC++STL.html though code is different
+			num_edges++;		
+		}	
+	}
+} 
+	
+EdgeWeight ListGraph::weight(NodeID u, NodeID v) const{	
+	std::list<NWPair>::const_iterator iter;
+	//search all u's edges
+	for(iter = edgeList[u].begin(); iter != edgeList[u].end(); iter++){			
+		if(iter->first == v){				
+			return iter->second;			
+		}else return 0;	
+	}	
+}
+	
+std::list<NWPair> ListGraph::getAdj(NodeID u) const{		
+		return edgeList[u];		
+}
+unsigned ListGraph::degree(NodeID u) const{		
+		return getAdj(u).size();
+}
+
+unsigned ListGraph::size() const{	
+		return edgeList.size();
+}
+unsigned ListGraph::numEdges() const{	
+		return num_edges;
+} 
